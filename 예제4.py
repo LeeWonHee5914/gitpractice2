60ea52f02875cb5a4882502a2bcb7829f0c61c43
이원희 <iwonhui@iwonhuiui-MacBookPro.local>
2019-10-21 14:17:28 +0900

    파이썬 예제풀이

---
+++ 예제4.py
@@ -0,0 +1,8 @@
+print( "you" "need" "python")
+youneedpython
+print("you"+"need"+"python")
+youneedpython
+print("you", "need", "pythob") # 콤마가 있는경우 공백이 삽입되어 더해진다.
+you need python
+print("".join(["you", "need", "python"]))
+youneedpython
